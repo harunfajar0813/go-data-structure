443ed72b78780cc33c87e6a3c5562a47d41983a5
harunfajar0813 <spam.harun0813@gmail.com>
2020-03-26 20:06:20 +0700

    Fix delete node and making new circularLinked

--- linear/linkedlist/linked.go
+++ linear/linkedlist/linked.go
@@ -1,4 +1,4 @@
-package linkedlist
+package main
 
 import "fmt"
 
@@ -20,60 +20,101 @@ type linked struct {
 	length int
 }
 
+func (l *linked) circularConnection() {
+	l.head.previous = l.tail
+	l.head.previous.next = l.head
+}
+
 func (l *linked) insert(value int) {
 	if err, newNode := newNode(value); err == nil {
 		if l.tail == nil {
-			l.head, l.tail = newNode, newNode
+			l.tail = newNode
+			l.head = l.tail
 		} else {
 			newNode.next = l.head
 			newNode.next.previous = newNode
 			l.head = l.head.previous
 		}
+		l.circularConnection()
 		l.length++
-	}
-}
-
-func (l *linked) deleteHead() error {
-	if l.head == nil {
-		return fmt.Errorf("list is empty")
-	} else {
-		l.head = l.head.next
-		l.head.previous = nil
-		return nil
 	}
 }
 
 func (l *linked) append(value int) {
 	if err, newNode := newNode(value); err == nil {
 		if l.tail == nil {
-			l.head, l.tail = newNode, newNode
+			l.tail = newNode
+			l.head = l.tail
 		} else {
 			newNode.previous = l.tail
 			newNode.previous.next = newNode
 			l.tail = l.tail.next
 		}
+		l.circularConnection()
 		l.length++
 	}
 }
 
-func (l *linked) deleteTail() error {
-	if l.tail == nil {
+func (l *linked) isEmpty() bool {
+	return l.tail == nil
+}
+
+func (l *linked) isOnlyOneNode() bool {
+	return l.tail == l.head
+}
+
+func (l *linked) deleteHead() error {
+	if l.isEmpty() {
 		return fmt.Errorf("list is empty")
 	} else {
-		l.tail = l.tail.previous
-		l.tail.next = nil
-		return nil
+		if l.isOnlyOneNode() {
+			l.head, l.tail = nil, nil
+		} else {
+			l.head = l.head.next
+			l.head.previous = nil
+			l.circularConnection()
+		}
 	}
+	l.length--
+	return nil
+}
+
+func (l *linked) deleteTail() error {
+	if l.isEmpty() {
+		return fmt.Errorf("list is empty")
+	} else {
+		if l.isOnlyOneNode() {
+			l.tail, l.head = nil, nil
+		} else {
+			l.tail = l.tail.previous
+			l.tail.next = nil
+			l.circularConnection()
+		}
+	}
+	l.length--
+	return nil
 }
 
 func (l *linked) show() {
-	for current := l.head; current != nil; current = current.next {
+	if l.isEmpty() {
+		fmt.Errorf("list is empty")
+		return
+	}
+	current := l.head
+	for index := 0; index < l.length; index++ {
 		fmt.Print(current.value, " -> ")
+		current = current.next
 	}
 }
 
 func (l *linked) showReverse() {
-	for current := l.tail; current != nil; current = current.previous {
+	if l.isEmpty() {
+		fmt.Errorf("list is empty")
+		return
+	}
+	current := l.tail
+	for index := 0; index < l.length; index++ {
 		fmt.Print(current.value, " -> ")
+		current = current.next
 	}
 }
